241f91a80c434f2f9d5bee9bb8f158ba44986980
brendandebeasi <b@debea.si>
2013-04-01 16:16:56 -0700

    Added everything

---
+++ wordpress/wp-content/themes/massnailit/sidebar.php
@@ -0,0 +1,29 @@
+                <div class="large-4 column">
+                    <form class="short-form custom custom-csl">
+                        <fieldset>
+                            <h3>Get Certified Today!</h3>
+                      
+                            <input type="text" name="firstName" placeholder="Name" />                       
+                   
+                            <input name="email" placeholder="Email Address" type="text" />
+                        
+                    
+                            <select name="course" class="custom dropdown large">
+                                <option value="online">Online CSL Course</option>
+                                <option value="offline">Real-World CSL Course</option>
+                            </select>
+                        
+                  
+                        
+                            <select name="course" class="custom dropdown large">
+                                <option value="12">12 Hour CSL Certification</option>
+                                <option value="10">10 Hour CSL Certification</option>
+                                <option value="6">6 Hour CSL Certification</option>
+                            </select>
+                        
+                    
+                            <a href="#" class="button expand button-orange">Get Certified Now!</a>
+                               
+                        </fieldset>
+                    </form>
+                </div>
