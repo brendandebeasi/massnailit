cc2fd2dd9216676981ef8ca25cdcd848c40dcdbd
brendandebeasi <b@debea.si>
2013-04-01 18:30:59 -0700

    Added new env - dev

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -4,7 +4,7 @@
 if (strpos($_SERVER['HTTP_HOST'], 'dev.massnailit.com') !== false && $_SERVER['HTTP_HOST'] == 1337) {
     $config_file = 'config/dev.php';
 }
-elseif (strpos($_SERVER['HTTP_HOST'], 'dev.mni.neueway.com') !== false && strpos($_SERVER['HTTP_HOST'], 'dev.neueway.com') !== false) {
+elseif (strpos($_SERVER['HTTP_HOST'], 'dev.mni.neueway.com') !== false ||  strpos($_SERVER['HTTP_HOST'], 'dev.neueway.com') !== false) {
     $config_file = 'config/dev3.php';
 }
 elseif (strpos($_SERVER['HTTP_HOST'], '127.0.0.1') !== false) {
