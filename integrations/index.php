d35a3e48d02263b40961a7bfeb6d8201c439325f
brendandebeasi <b@debea.si>
2013-04-17 14:43:15 -0700

    You can now create a user using the integration server

--- integrations/index.php
+++ integrations/index.php
@@ -2,13 +2,12 @@
 /*
  * MassNailIt Integrations File
  * This file can perform the following functions:
- * TODO:  get list of a certain type of products from IS
+ * - get list of a certain type of products from IS
  * TODO:  link a user in litmos with a user in infusion soft
  * TODO:  authenticate a litmos user to use a certain program
  */
 require_once('src/conn.cfg.php');
 require_once('src/isdk.php');
-require_once('src/pest/PestJSON.php');
 
 
 if(isset($_GET['action'])) $action = $_GET['action'];
@@ -24,17 +23,19 @@ switch($action) {
     case 'create-litmos-user':
         if(isset($_GET['name_first']) && isset($_GET['name_last']) && isset($_GET['email_address']) ) {
             $name_first = $_GET['name_first'];
-            $name_last= $_GET['name_first'];
+            $name_last= $_GET['name_last'];
             $email= $_GET['email_address'];
-            $user_response = LM_createUser($name_first, $name_last, $email);
-            print_r($user_response);
+            $user_response = LM_createUser($name_first, $name_last, $email,$username=null);
+            echo json_encode($user_response);
         }
+        break;
     default:
         die("Invalid action set");
 }
 
 
 function IS_getProductsWithPrefix($prefix=false) {
+    $response = ['success'=>0,'message'=>"An unknown error occured."];
     $app = new iSDK;
     $app->cfgCon("mni");
 
@@ -46,20 +47,46 @@ function IS_getProductsWithPrefix($prefix=false) {
     $products = $app->dsQuery("Product",10,0,$query,$returnFields);
     $products_json = json_encode($products);
     if(json_decode($products_json) === null) {
-        die('Invalid Json Returned' . $products);
+        $response = ['success'=>0,'message'=>"Invalid JSON returned: " . $products];
+
     }
-    else return $products_json;
+    $response = ['success'=>1,'message'=>null,'data'=>$products_json];
+    return $response;
 }
 
-function LM_createUser($first_name,$last_name,$email_address) {
+function LM_createUser($first_name,$last_name,$email_address,$username) {
+    $response = ['success'=>0,'message'=>"An unknown error occured."];
 
+    require_once('src/pest/PestJSON.php');
+    if(is_null($username)) {
+        $username = $email_address;
+        $is_custom_user = false;
+    }
+    else {
+        $is_custom_user = true;
+    }
     $pest = new PestJSON('https://api.litmos.com/v1.svc');
-    $data = Array(
+    $data = [
         'Email' => $email_address,
+        'UserName'=>$username,
         'FirstName' => $first_name,
         'LastName' => $last_name,
-        'SkipFirstLogin'=>true
-    );
-    $user = $pest->post('/users?apikey=E8C3D63F-A273-461A-9691-37FC53EED941',$data);
-    return $user;
+        'SkipFirstLogin'=>true,
+        'DisableMessages'=>false,
+        'IsCustomUsername'=>$is_custom_user,
+        'Password'=> 'asdfas',
+        'Active'=>true
+    ];
+    try {
+        $user = $pest->post('/users?apikey=E8C3D63F-A273-461A-9691-37FC53EED941&source=mni',$data);
+        $response = ['success'=>1,'message'=>null,'data'=>['response'=>json_decode($user),'id'=>$user['Id']]];
+    }
+    catch(Pest_Conflict $e) {
+        $msg = json_decode($e->getMessage());
+        if(isset($msg->Detail)) $msg = $msg->Detail;
+        else $msg = 'An unknown error occurred.';
+        $response = ['success'=>0,'message'=>$msg];
+    }
+
+    return $response;
 }
