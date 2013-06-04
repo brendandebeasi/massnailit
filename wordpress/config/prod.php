06622522f6472575995217a482828af1ddd1c757
Brendan DeBeasi <brendan.debeasi@nfl.com>
2013-06-03 20:59:38 -0700

    Fixed name of site

--- wordpress/config/prod.php
+++ wordpress/config/prod.php
@@ -3,5 +3,5 @@ define('VIA_ENVIRONMENT', 'prod');
 define('DB_NAME', 'mni_' . VIA_ENVIRONMENT);
 define('DB_USER', 'mni_' . VIA_ENVIRONMENT);
 define('DB_PASSWORD', 'mni_' . VIA_ENVIRONMENT . '_3dfd45234fg');
-//define('WP_SITEURL', 'https://www.massnailit.com');
-//define('WP_HOME', 'https://www.massnailit.com');
+define('WP_SITEURL', 'https://www.massnailit.com');
+define('WP_HOME', 'https://www.massnailit.com');
