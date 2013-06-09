25908811e51de67152e92cecd1c7c1ce3bd748b9
Brendan DeBeasi <brendan.debeasi@nfl.com>
2013-06-09 13:46:41 -0700

    Replaced swipe func

--- wordpress/wp-content/themes/massnailit/header.php
+++ wordpress/wp-content/themes/massnailit/header.php
@@ -88,7 +88,7 @@
         <header>
             <div class="row">
                 <div class="large-5 columns">
-                    <div class="mobileMenuToggle menu-trigger ">MENU</div>
+                    <div class="mobileMenuToggle">MENU</div>
                     <a class="brand" href="<?php bloginfo('wpurl'); ?>">
                         <div class="icon-logo"></div>
                         <div class="icon-brand"><img src="<?php bloginfo('stylesheet_directory'); ?>/css/img/mass-nail-it.png" alt="Mass Nail It"/></div>
@@ -103,10 +103,11 @@
                 </div>
             </div>
                 <nav class="nav-background">
-                    <div id="menu" class="large-12 column row top-bar-section">
+                    <div class="large-12 column row top-bar-section">
                         <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'menu_class' =>'left' ) ); ?>
                     </div>
                 </nav>
 
         </header>
+    <div class="mobileMenu"><?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'menu_class' =>'mobile' ) ); ?></div>
 
